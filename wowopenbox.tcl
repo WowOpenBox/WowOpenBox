605dd59ed86d5c98e52a41bc94b3806c7a7b2bb7
Moorea Tv <moorea@ymail.com>
2020-11-22 09:00:53 -0800

    working placement

--- wowopenbox.tcl
+++ wowopenbox.tcl
@@ -3,11 +3,28 @@ package require twapi
 # https://twapi.magicsplat.com/v4.4/ui.html
 
 puts [twapi::get_desktop_workarea]
+puts [twapi::get_display_monitors -activeonly]
+puts [twapi::get_multiple_display_monitor_info]
 
-set w [twapi::find_windows -text "World of Warcraft" -popup false -single]
 
-puts $w
+proc FindWow {} {
+    set w [twapi::find_windows -text "World of Warcraft" -visible true -single]
+    return $w
+}
 
-# hide (most of the) title bar
-twapi::configure_window_titlebar $w -visible false
-# find how to get rid of border
+proc RenameResize {w i} {
+    twapi::resize_window $w 1920 1080
+    twapi::set_window_text $w "WoW $i"
+}
+
+proc Move {w x y} {
+    twapi::move_window $w $x $y
+}
+
+
+set w [FindWow]
+RenameResize $w 1
+Move $w -3840 -720
+RenameResize $w 2
+Move $w -3840 360
+# ... etc...
