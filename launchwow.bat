3cf84f62f2381799d7199c364f32178a3c51d989
Moorea Tv <moorea@ymail.com>
2021-03-14 14:34:03 -0700

    fixed script so saving changes in each window actually works as well better copy only if not exist; added BIN for WowClassic launch

--- launchwow.bat
+++ launchwow.bat
@@ -1,3 +1,4 @@
+@echo off
 REM    If you prefer to launch WoW without using the 
 REM    recomended battle.net way (which avoids password typing)
 REM    you can use this instead.
@@ -9,8 +10,11 @@ set HOWMANY=5
 REM Where to find and which wow (classic, retail,...)
 REM Change for instance to _classic_
 set WOWDIR=C:\Program Files (x86)\World of Warcraft\_retail_
+REM Exe name: Wow.exe or WowClassic.exe
+set BIN=Wow.exe
+echo Will launch %HOWMANY% %BIN% in WOWDIR=%WOWDIR%
 FOR /L %%i IN (1,1,%HOWMANY%) DO (
-    echo %%i
-    echo n | copy /-y "%WOWDIR%\WTF\Config.wtf" "%WOWDIR%\WTF\Config-WOB%%i.wtf"
-    start "" "%WOWDIR%\Wow.exe" -config "%WOWDIR%\WTF\Config-WOB%%i.wtf"
+    echo Launching #%%i
+    IF NOT EXIST "%WOWDIR%\WTF\Config-WOB%%i.wtf" copy "%WOWDIR%\WTF\Config.wtf" "%WOWDIR%\WTF\Config-WOB%%i.wtf" >NUL
+    start "" "%WOWDIR%\%BIN%" -config Config-WOB%%i.wtf
 )
